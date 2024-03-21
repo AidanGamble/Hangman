9e34e1613d74afa60203a6aa0d19e0915a8b917d
AidanGamble <aga99@sfu.ca>
2024-03-20 18:02:42 -0700

    Multiplayer added
    Game supports 1 or 2 players.  1 player phrase is default, in multiplayer, one player sets the word.

--- hangman.py
+++ hangman.py
@@ -8,12 +8,43 @@ CONST_ASCII_TO_INDEX = 65
 #   phrase.
 
 def initializeGame():
-    phrase = "ABCDEF"
+    phrase = "THIS IS SOMEONE'S TEST"
+    phrase = intro(phrase).upper()
+    os.system("cls")
+
     foundLetters = [False for i in range(len(phrase))]
+
+    #Set found letter to true if it is not alpha
+    for i in range(len(foundLetters)):
+        if(not phrase[i].isalpha()):
+            foundLetters[i] = True
+
     alreadyGuessed = [False for i in range(26)]
     numMisses = 5
+    drawHangman(numMisses)
     printRevealed(phrase, foundLetters, alreadyGuessed)
     return [phrase, foundLetters, alreadyGuessed, numMisses]
+
+def intro(phrase):
+    while(True):
+        x = input("Welcome to hangman! Enter '1' for singleplayer and '2' for two player: ")
+        if(x == "1"):
+            return phrase
+        if(x == "2"):
+            while(True):
+                y = input("Player 1, enter your secret phrase: ")
+                for i in range(len(y)):
+                    if(y[i].isalpha()):
+                        return y
+                
+                os.system("cls")
+                print("Invalid entry. Phrase must have at least 1 letter.")
+        else:
+            os.system("cls")
+            print("Invalid input.")
+        
+        
+        
 
 def parseInput(alreadyGuessed):
     x = input("Enter your guessed letter: ")
@@ -36,7 +67,10 @@ def printRevealed(phrase, foundLetters, alreadyGuessed):
         if(foundLetters[i]):
             print(phrase[i], end=" "),
         else:
-            print("_", end=" ")
+            if(phrase[i].isalpha()):
+                print("_", end=" ")
+            else:
+                print(phrase[i], end = " ")
     print("")
     print("Guessed Letters: ", end=" ")
     for i in range(26):
@@ -54,6 +88,28 @@ def updateLetters(letter, phrase, foundLetters):
             foundLetters[i] = True
     return foundLetters
 
+def drawHangman(numMisses):
+    print("   ____")
+    print("   |  |")
+    if(numMisses >= 5):
+        print("   |")
+    else:
+        print("   |  o")
+    if(numMisses >=4):
+        print("   |")
+    elif(numMisses >= 3):
+        print("   |  |")
+    elif( numMisses >= 2):
+        print("   |  |\\")
+    else:
+        print("   | /|\\")
+    if(numMisses >= 1):
+        print("   |")
+    else:
+        print("   | / \\")
+    
+    print("___|___")
+
 
 def main():
 
@@ -65,14 +121,23 @@ def main():
         if(guessedLetter in phrase):
             foundLetters = updateLetters(guessedLetter, phrase, foundLetters)
             os.system("cls")
+            drawHangman(numMisses)
             printRevealed(phrase, foundLetters, alreadyGuessed)
             print("Hit!")
         else:
             os.system("cls")
-            printRevealed(phrase, foundLetters, alreadyGuessed)
             numMisses -= 1
+            drawHangman(numMisses)
+            printRevealed(phrase, foundLetters, alreadyGuessed)
             print("Miss! " + str(numMisses) + " chances left.")
-        
+    
+    if(not (False in foundLetters)):
+        print("You win! Press enter to exit.")
+    else:
+        print("You lose! The answer was " + phrase + ". Press enter to exit.")
+    
+    #Blank input to wait before closing game
+    y = input()
     #Clear console before exiting
     os.system("cls")
 
